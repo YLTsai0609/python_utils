22f1ff928fecba3132af199e1e584f21597c06a8
YLTsai0609 <yltsai0609@gmail.com>
2021-03-29 00:21:54 +0800

    [Fix] fix now() in datetime_stuff.py

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # python utils
 
-This repo include reuseable python utils including `thread` , `io` , `logging` things from yu long's experience.
+This repo include reuseable python utils including `thread` , `io` , `logging`, `os`, `datetime` things from yu long's experience.
 
 # TODO
 
-* [ ] collect code
 * [ ] normalize documentation in the functions
 * [ ] add tests
--- utils/datetime_stuff.py
+++ utils/datetime_stuff.py
@@ -4,12 +4,12 @@ import pendulum
 
 
 def get_datespan_int(
-    start = pendulum.date(2020, 12, 20),
-    end = pendulum.date(2021, 1, 29)) -> List[int]:
+    start=pendulum.date(2020, 12, 20), end=pendulum.date(2021, 1, 29)
+) -> List[int]:
     datespan = pendulum.period(start, end)
-    datespan_int = [
-        int(d.format('YYYYMMDD'))
-        for d
-        in datespan.range("days",1)
-    ]
+    datespan_int = [int(d.format("YYYYMMDD")) for d in datespan.range("days", 1)]
     return datespan_int
+
+
+def now(fmt="%m-%d-%H-%M-%S") -> str:
+    return pendulum.now().strftime(fmt)
