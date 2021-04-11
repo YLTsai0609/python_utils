6172b532ff3fb528e556af9029330fd6443beb9a
YLTsai0609 <yltsai0609@gmail.com>
2021-04-11 17:08:48 +0800

    [Feature] add datespan int range_unit and range_num

--- utils/datetime_stuff.py
+++ utils/datetime_stuff.py
@@ -4,10 +4,15 @@ import pendulum
 
 
 def get_datespan_int(
-    start=pendulum.date(2020, 12, 20), end=pendulum.date(2021, 1, 29)
+    start=pendulum.date(2020, 12, 20),
+    end=pendulum.date(2021, 1, 29),
+    range_unit: str = "days",
+    range_num: int = 1,
 ) -> List[int]:
     datespan = pendulum.period(start, end)
-    datespan_int = [int(d.format("YYYYMMDD")) for d in datespan.range("days", 1)]
+    datespan_int = [
+        int(d.format("YYYYMMDD")) for d in datespan.range(range_unit, range_num)
+    ]
     return datespan_int
 
 
